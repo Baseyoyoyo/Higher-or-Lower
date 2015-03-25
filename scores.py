4a9ac9d194622651d5ad6ba335d0407bc674ecd0
Harvey <hevans.2000@gmail.com>
2015-03-25 12:27:06 +0000

    Allow user to view either most recent scores or high scores, referance #4

--- scores.py
+++ scores.py
@@ -1,12 +1,20 @@
 import sqlite3
 
+
 db = sqlite3.connect("scores.db")
 
 cursor = db.cursor()
 
-scores = cursor.execute("""
-  select name, score, recorded_at from scores order by score desc;
-""").fetchall()
+print ("If you wish to see the top scores from the past 10 days then please enter recent if you would like to see the all time high scores then please enter all.")
 
-for score in scores:
+choice = raw_input ("")
+
+sort_field = "recorded_at" if choice == "recent" else "score"
+
+scores = cursor.execute(
+  "SELECT name, score, recorded_at FROM scores ORDER BY " + sort_field + " DESC LIMIT ?", 
+  (10,)
+)
+
+for score in scores.fetchall():
   print "player " + score[0] + " achieved a score of " + str(score[1]) + " at " + str(score[2])
