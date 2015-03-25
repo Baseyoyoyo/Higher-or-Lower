ff02e58b49effc24c31ddf57470206edcf25a925
Harvey <hevans.2000@gmail.com>
2015-03-25 16:10:01 +0000

    Add player score and scoreboard classes, reference #5

--- Game.py
+++ Game.py
@@ -1,26 +1,19 @@
 # Simple CLI based game for guessing a number being higher or lower
 # Requires Python > 3
 # `python higher_or_lower.py`
-import sqlite3
 import random
-import datetime
+from lib.score_board import ScoreBoard
+from lib.player_score import PlayerScore
 
-db = sqlite3.connect("scores.db")
-
-cursor = db.cursor()
-
-cursor.execute("CREATE TABLE IF NOT EXISTS scores(id INTEGER PRIMARY KEY, name TEXT, score INTEGER, recorded_at DATETIME)")
-
+score_board = ScoreBoard()
 
 print ("Hello and welcome to my higher or lower number guessing game.")
 
-myName = raw_input("Whats your name? ")
+name = raw_input("Whats your name? ")
 
-recorded_at = datetime.datetime.now()
+print ("So, your names " + name + " Hmmmmmmm")
 
-print ("So, your names " + myName + " Hmmmmmmm")
-
-score = 0
+score = PlayerScore()
 count = 1
 previousNumber = random.randrange(1, count*20)
 
@@ -36,14 +29,13 @@ while count <= 15:
   guess = raw_input("\n\nEnter either h or l: ")
 
   if (previousNumber > number and guess == "l") or (number > previousNumber and guess == "h"):
-    score = score + 1
+    score.increment()
 
     print ("Well done the number was " + str(number) + " Your score at the end of the round is " + str(score))
 
   else:
 
-    if score > 0:
-      score = score - 1
+    score.decrement()
 
     print ("Incorrect the number was " + str(number) + " Your score at the end of the round is " + str(score))
 
@@ -52,16 +44,8 @@ while count <= 15:
   count = count + 1
   
 
+score_board.add(name, score.value)
 print ("Well done your score is " + str(score) + " Good Job")
-
-cursor.execute(
-  "INSERT INTO scores(name, score, recorded_at) VALUES(?,?,?)", 
-  (myName, score, recorded_at)
-)
 
 execfile("scores.py")
 
-
-db.commit()
-
-db.close()
